4f0d2c8392b99b45e36025a578ca1f7cd5584645
chaitanyakumar-d <chaitanyakumar435@outlook.com>
2025-04-10 15:19:29 -0500

    modified test file

--- tests/test_summary.py
+++ tests/test_summary.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../scripts')))
+
 from scripts.chatgpt_utils import summarize_text
 
 def test_summary():
